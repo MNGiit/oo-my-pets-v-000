322b11573e22f8212d6114ae54718561aa07b558
Mohammed Noori <kmanoorib@gmail.com>
2018-06-24 03:07:29 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -50,4 +50,12 @@ class Owner
   def feed_fish
     @pets[:fishes].each {|fish| fish.mood = "happy"}
   end
+  
+  def sell_pets
+    @pets.each do |species|
+      species.each {|pet| pet.mood = "nervous"}
+    end
+  end
+      
+  
 end
