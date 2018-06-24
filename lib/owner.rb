e027f023ea839266884d851d6b366c80015c4f0d
Mohammed Noori <kmanoorib@gmail.com>
2018-06-24 02:58:53 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,14 +1,14 @@
 class Owner
   # code goes here
   attr_accessor :name
-  attr_reader :species
+  attr_reader :species, :pets
   @@all = [] #keeps track of the owners that have been created
   
   def initialize(name)
     @name = name
     @@all << self #add self to the array
     @species = "human"
-    
+    @pets = {fishes => [], dogs => [], cats => []}
   end
   
   def self.all #shows all owners
