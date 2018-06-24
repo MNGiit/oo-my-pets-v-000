f46c5fa8c2f5e6799f740149599c9d4b054563e6
Mohammed Noori <kmanoorib@gmail.com>
2018-06-24 02:51:39 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -3,6 +3,10 @@ class Owner
   
   @@all = [] #keeps track of the owners that have been created
   
+  def initialize(name)
+    @name = name
+  end
+  
   def self.all #shows all owners
     @@all
   end
